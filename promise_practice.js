1242a8e18a23cfb2a6d187cf761b0ad62209b68c
Charmi Vadaliya <charmivadaliya102@gmail.com>
2022-01-06 12:01:53 +0530

    updated promise_practice.js

--- promise_practice.js
+++ promise_practice.js
@@ -1,14 +1,41 @@
-function func1(){
+const students = [
+    {name: "Charmi", subject:"Javascript"},
+    {name: "Nemi", subject:"Biology"}
+]
+
+function enrollStudent(student){
+    
     return new Promise(function(resolve,reject){
-        setTimeout(() => {
-            const error = true;
+        setTimeout(function(){
+            students.push(student);
+            console.log('Student enrolled');
+            const error = false;
             if(!error){
-                console.log('Your promise is resolved')
                 resolve();
             }else{
-                console.log('your promise is not resolved')
-                reject('Sorry'); 
+                reject();
             }
+            
         }, 2000);
-    })
-} 
+        
+    })  
+    
+}
+
+function getStudents(){
+    setTimeout(function(){
+        let str="";
+        students.forEach(function(student){
+            str+=`<li> ${student.name}</li>`
+        });
+        document.getElementById('students').innerHTML = str;
+        console.log("students have been fetched");
+    }, 1000);
+}
+
+let newStudent = {name:"Bansi" , subject:"python"}
+enrollStudent(newStudent).then(getStudents).catch(function(){
+    console.log("Eroor occured")
+});
+
+ 
